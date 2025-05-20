8c5c0b309143f9cd58ec2aebe58a3f9213f7dbd0
Mikel <devops@t.log>
2025-05-20 23:47:36 +0700

    refactor: macos with playbooks install starship and lsd

--- macos/install_app_store.sh
+++ macos/install_app_store.sh
@@ -2,4 +2,4 @@
 sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-python3 -m ansible playbook playbook.yml --tags appstore
+ansible-playbook playbook.yml --tags appstore
